22218eeac1fd49d2fe672db63fa6bcfa2ddb18e4
Ian Grunert <igrunert@atlassian.com>
2015-07-10 16:10:13 +1000

    Fixing match glob pattern.

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@ DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
 cd "$DIR"
 
 # Get latest tag
-VERSION=$(git describe  --abbrev=0 --tags --match="v.*")
+VERSION=$(git describe  --abbrev=0 --tags --match="v*")
 
 # Determine the arch/os combos we're building for
 XC_ARCH=${XC_ARCH:-"386 amd64 arm"}
